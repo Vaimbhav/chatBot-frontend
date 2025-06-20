c9fe8283a822b3fffc2120616731544056e4003a
Vaibhav Yadav <vaimbhav05@gmail.com>
2025-06-20 13:07:27 +0530

    fix: set base to root for Netlify

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	base: '/', // ✅ Must be '/' for Netlify root domain
 	server: {},
-	base: process.env.VITE_BASE_PATH || '/chatBot-frontend',
 	build: {
-		sourcemap: false, // Disable source maps
+		sourcemap: false,
 	},
 });
